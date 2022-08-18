4ffe14c4ae99185ccd3537c62abe3e77feb84a65
pscheol <nelfiel12@gmail.com>
2022-08-18 17:07:23 +0900

    add android

---
+++ android/app/src/main/java/com/pscheol/ionicsample/MainActivity.java
@@ -0,0 +1,5 @@
+package com.pscheol.ionicsample;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.pscheol.ionicsample',
   appName: 'IonicSampleApp',
   webDir: 'dist',
   bundledWebRuntime: false
