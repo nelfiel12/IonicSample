e617187459e42e08496cb18e3e6ca41aae4f9aa3
pscheol <nelfiel12@gmail.com>
2022-08-22 13:53:41 +0900

    webview version toast

--- android/app/src/main/java/com/pscheol/ionicsample/MainActivity.java
+++ android/app/src/main/java/com/pscheol/ionicsample/MainActivity.java
@@ -1,6 +1,11 @@
 package com.pscheol.ionicsample;
 
+import android.content.pm.PackageInfo;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.webkit.WebViewCompat;
+
 import com.getcapacitor.BridgeActivity;
 import com.pscheol.ionicsample.plugins.PhotoGalleryPlugin;
 
@@ -9,5 +14,9 @@ public class MainActivity extends BridgeActivity {
     protected void onCreate(Bundle savedInstanceState) {
         registerPlugin(PhotoGalleryPlugin.class);
         super.onCreate(savedInstanceState);
+
+        PackageInfo info = WebViewCompat.getCurrentWebViewPackage(getApplicationContext());
+
+        Toast.makeText(getApplicationContext(), "WebView version : " + info.versionName, Toast.LENGTH_LONG).show();
     }
 }
